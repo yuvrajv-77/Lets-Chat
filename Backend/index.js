209f0e35d92a0711d6dad79873d6345427ecd634
Yuvraj Verma <y74125@gmail.com>
2024-09-26 12:57:17 +0530

    Update index.js cors

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,24 @@ const MessageRoutes = require("./Routes/MessageRoutes");
 
 const app = express();
 const server = createServer(app);
-app.use(cors())
+
+const allowedOrigins = [
+  'https://lets-chat-k4dr.onrender.com',
+  'http://localhost:5173'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(bodyParser.json());
 app.use(express.json());
 
@@ -41,8 +58,8 @@ server.listen(process.env.PORT || 6001, () => {
 
 const io = new Server(server, { 
     cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
+        origin: allowedOrigins,
+    methods: ["GET", "POST"]
         credentials: true
     }
  });
